3c2a0a7bbe5e369568d4f3ba4b4a7eb795aa36e1
andres gonzalez <andresf549@gmail.com>
2026-02-05 19:25:01 -0500

    primer archivo del repositorio

---
+++ app1.py
@@ -0,0 +1,21 @@
+# print("hello word ")
+
+# para hacer un ciclo for 
+#a=2
+#for i in range(0,10):
+
+   # print("sigo en  el ciclo ") 
+ #   print(i)
+#print("aqui termina el condicional ") 
+
+
+class Estudiante:
+    def __init__(self,nombre, documento):
+        self.nombre= nombre 
+        self.documento= documento
+
+estudiante1 = Estudiante("juan",111)
+print(estudiante1.nombre)
+
+
+        
